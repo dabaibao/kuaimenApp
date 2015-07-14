051fac1fd81eb51514918411acea0c2eee8094a1
dabaibao <serendipity20050811@hotmail.com>
2015-07-14 22:14:29 +0800

    Initial Commit

---
+++ kuaimenApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  kuaimenApp
+//
+//  Created by 姬鹏 on 15/7/14.
+//  Copyright (c) 2015年 姬鹏. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
